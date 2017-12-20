1976e799a51d40507ca32f129ed11cb5b96c9beb
Nat Karmios <NKarmios@live.co.uk>
2017-12-20 05:11:16 +0000

    Added cloneObject, getDataDir, readFile and writeFile to helper_funcs

--- app/_modules/utils/helper_funcs.js
+++ app/_modules/utils/helper_funcs.js
@@ -1,12 +1,28 @@
+import fs from 'fs';
 import { remote, BrowserWindow } from 'electron';
 
-export const delay = (t: number): Promise<void> => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, t);
-  });
-};
+const { app } = remote;
 
-// eslint-disable-next-line arrow-body-style
-export const getWindow = (): BrowserWindow => {
-  return remote.getCurrentWindow();
-};
+
+export const delay: number => Promise<void> =
+  t => new Promise((resolve) => { setTimeout(resolve, t); });
+
+export const getWindow: () => BrowserWindow =
+  () => remote.getCurrentWindow();
+
+export const cloneObject: <T>(t) => T =
+    obj => JSON.parse(JSON.stringify(obj));
+
+export const getDataDir: () => string =
+  () => app.getPath('userData');
+
+export const readFile: string => string =
+  filename => new Promise((resolve, reject) => fs.readFile(filename,
+    (data, err) => ((err !== null && err !== undefined) ? resolve(data) : reject(err))
+  ));
+
+export const writeFile: (string, string) => void =
+  (filename, contents) => new Promise((resolve, reject) => fs.writeFile(filename, contents,
+    err => ((err !== null && err !== undefined) ? resolve() : reject(err))
+  ));
+
