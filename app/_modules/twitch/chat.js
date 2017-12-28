a1b206ff4cb4031f81c294614f218607744b4503
Nat Karmios <NKarmios@live.co.uk>
2017-12-28 16:42:56 +0000

    Modified _modules/twitch/chat.js to use a queue
    The moduie now exposes queueMessage() rather than sendMessage(), as other modules should not be able to circumvent the queue.

--- app/_modules/twitch/chat.js
+++ app/_modules/twitch/chat.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { getSetting, tempVars } from '../persist/settings';
+import { IntervalQueue } from '../../utils/IntervalQueue';
 
 const TWITCH_CHAT_URL = 'wss://irc-ws.chat.twitch.tv:443';
 const PREFIX = '/me - ';
@@ -8,36 +9,48 @@ const LOG_TWITCH = false;
 let connectionStarted = false;
 const getChannel = () => { return `#${tempVars.streamerNick}`; };
 const getNick = () => { return tempVars.botNick; };
+let queue: IntervalQueue;
 
 let socket: WebSocket;
-const listeners: Array = [
+const listeners: Array<string => void> = [
   // The Twitch IRC server will send a PING message, which must reply with PONG
   // to prove that the bot is not inactive.
   msg => { if (msg === 'PING :tmi.twitch.tv') sendRaw('PONG :tmi.twitch.tv'); }
 ];
 if (LOG_TWITCH) listeners.push(msg => { console.log(`< ${msg}`); });
 
+
+// <editor-fold desc="Sending Functions">
+
 export const sendRaw = msg => {
   if (LOG_TWITCH) console.log(`> ${msg}`);
   socket.send(msg);
 };
 
-export const sendMessage = (msg, prefix=true) => {
+const sendMessage = (msg) => {
   sendRaw(`:${getNick()}!${getNick()}@${getNick()}.tmi.twitch.tv PRIVMSG 
-           ${getChannel()} :${prefix ? PREFIX : ''}${msg}\r\n`);
+           ${getChannel()} :${msg}\r\n`);
 };
+
+export const queueMessage = (msg, prefix=true) => {
+  queue.put(`${prefix ? PREFIX : ''}${msg}`);
+};
+
+// </editor-fold>
+
 
 const login = () => {
   sendRaw(`PASS oauth:${getSetting('login', 'botAuthToken')}`);
   sendRaw(`NICK ${getNick()}`);
   sendRaw(`JOIN ${getChannel()}`);
+  sendRaw('CAP REQ :twitch.tv/tags');
 };
 
 export const close = () => {
   socket.close();
 };
 
-export const onMessage = (func: () => void) =>  {
+export const onMessage = (func: string => void) =>  {
   listeners.push(func);
 };
 
@@ -48,9 +61,10 @@ export const connect = () => {
     socket = new WebSocket(TWITCH_CHAT_URL);
     socket.addEventListener('open', () => {
       login();
+      queue = new IntervalQueue(sendMessage);
       console.log('Connected to chat.');
       resolve();
     });
-    socket.addEventListener('message', event => { listeners.forEach(func => { func(event.data); }); });
+    socket.addEventListener('message', event => { listeners.forEach(func => func(event.data)); });
   });
 };
