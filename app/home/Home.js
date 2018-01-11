92aa5ac5c0bc91933ef969434a2dffc36a922821
Nat Karmios <NKarmios@live.co.uk>
2018-01-11 10:41:10 +0000

    Added some placeholder content to Home page

--- app/home/Home.js
+++ app/home/Home.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 import { MDIcon } from '../utils/components';
 import Typography from 'material-ui/es/Typography/Typography';
 
 const Home = () => (
-  <div>
-    <Paper style={{ margin: '20px', padding: '20px' }}>
+  <div style={{ margin: '20px' }}>
+    <Paper style={{ padding: '20px' }}>
       <Typography type="headline">
         <MDIcon color="black" style={{ marginRight: '5px' }}>home</MDIcon>
         Home
@@ -19,6 +20,19 @@ const Home = () => (
         </Link>
       </Typography>
     </Paper>
+    <br/>
+    <Grid container>
+      <Grid item xs={12} sm={6}>
+        <Paper style={{ padding: '20px' }}>
+          <Typography>[Question list]</Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Paper style={{ padding: '20px' }}>
+          <Typography>[Settings]</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
   </div>
 );
 
