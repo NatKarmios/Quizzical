7386f947212c3684765118c08865ee36db792f12
Nat Karmios <NKarmios@live.co.uk>
2018-02-12 23:15:35 +0000

    Added logic for deleting a question.

--- app/logic.js
+++ app/logic.js
@@ -1,11 +1,13 @@
 import global from './global/logic';
 import counter from './counter/counterLogic';
 import settings from './settings/settingsLogic';
-import questionList from './home/questionList/questionListLogic'
+import questionList from './home/questionList/questionListLogic';
+import questionDisplay from './home/questionDisplay/questionDisplayLogic';
 
 export default [
   ...global,
   ...counter,
   ...settings,
-  ...questionList
+  ...questionList,
+  ...questionDisplay
 ];
