4805aca24f89eaec31b1d86e1df63b6a1711f253
Nat Karmios <NKarmios@live.co.uk>
2018-02-14 22:17:08 +0000

    Injected Noty CSS into page

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './root/Root';
 import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import './app.global.scss';
 import { testSavedTokens } from './global/actions';
 import { setUpDB } from './_modules/db/dbSetup';
 import { loadSettings } from './global/actions/settingsActions';
