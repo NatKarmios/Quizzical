3e043aa62f2a28f40190ec74aff0cbb3493ec1e8
Nat Karmios <NKarmios@live.co.uk>
2018-02-23 15:24:35 +0000

    Removed useless types.js in app/_global/

--- app/_global/types.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import type { counterStateType } from '../counter/counterReducer';
-import type { NavbarStateType } from '../navbar/navbarReducer';
-
-export type GlobalStateType = {
-  ...counterStateType,
-  ...NavbarStateType
-};
