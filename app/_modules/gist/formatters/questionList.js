fc69ec15c02cca4bae7ac35613f96d523972d983
Nat Karmios <NKarmios@live.co.uk>
2018-03-07 09:56:14 +0000

    Commented formatters/questionList

--- app/_modules/gist/formatters/questionList.js
+++ app/_modules/gist/formatters/questionList.js
@@ -3,6 +3,12 @@
 import type { QuestionType } from '../../../utils/types';
 
 
+/**
+ *  Maps a question to a Markdown-readable format
+ *
+ * @param [unnamed] | The question to be mapped
+ * @returns The Markdown-formatted question info
+ */
 const mapQuestion = ({
   questionID, content, correctAnswer, incorrectAnswers, external
 }: QuestionType) =>
@@ -13,6 +19,15 @@ const mapQuestion = ({
     ...(external ? ['', '*This question was imported from the internet.*'] : [])
   ].join('\r');
 
+
+/**
+ *  Maps a list of questions to a Markdown-readable format
+ *
+ * @param questionList | The list of questions to be mapped
+ * @param page         | The current 'page' of questions
+ * @param maxPage      | The page number of the last 'page' of questions
+ * @returns The Markdown-formatted question list
+ */
 const questionListFormatter = (
   questionList: Array<QuestionType>, page: number, maxPage: number
 ) => [
