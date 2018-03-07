9025f60c4d3e6be52a9b216a224ec8d34ac0acfb
Nat Karmios <NKarmios@live.co.uk>
2018-03-07 09:55:41 +0000

    Commented formatters/commandList

--- app/_modules/gist/formatters/commandList.js
+++ app/_modules/gist/formatters/commandList.js
@@ -2,7 +2,12 @@
 
 import type { CommandType, CommandDetailsType } from '../../twitch/commands/parse';
 
-
+/**
+ *  Map command details to Markdown format
+ *
+ * @param commandDetails | The command details to be mapped
+ * @returns The Markdown-formatted details
+ */
 const mapCommand = (commandDetails: ?CommandDetailsType) => {
   if (commandDetails === null || commandDetails === undefined) {
     throw Error('Logic error!');
@@ -16,6 +21,14 @@ const mapCommand = (commandDetails: ?CommandDetailsType) => {
   ].join('\r');
 };
 
+
+/**
+ *  Transform a list of commands into a Markdown-readable list of
+ *  command information.
+ *
+ * @param commandList | The list of commands to be transformed
+ * @returns The Markdown-formatted command list
+ */
 const commandListFormatter = (commandList: Array<CommandType>) => [
   '*To use a command, either whisper it to me, or post it in chat prefixed with `?`.*',
   ...commandList
