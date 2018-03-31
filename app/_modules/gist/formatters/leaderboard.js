b8f5357bcaf43a8474bed6f2c10760a930156530
Nat Karmios <NKarmios@live.co.uk>
2018-03-31 11:51:30 +0100

    Commented gist/formatters/leaderboard

--- app/_modules/gist/formatters/leaderboard.js
+++ app/_modules/gist/formatters/leaderboard.js
@@ -2,17 +2,25 @@
 
 import type { WinnerTotalType } from '../../../utils/types';
 
-
-const leaderboardFormatter = (winnerList: Array<WinnerTotalType>) => [
-  `*Showing top ${winnerList.length} viewer${winnerList.length === 1 ? '' : 's'}.*`,
-  '',
-  '| Viewer | Wins |',
-  '| ------ | ---- |',
-  ...winnerList.map((winner: WinnerTotalType) =>
-    `| ${winner.name} | ${winner.total} |`
-  ),
-  ''
-].join('\n');
-
+/**
+ *  Transform a list of winner totals into a Markdown-readable table of
+ *  winner information.
+ *
+ * @param winnerList | The list of winners to be transformed
+ * @returns The Markdown-formatted winner table
+ */
+const leaderboardFormatter = (winnerList: Array<WinnerTotalType>) =>
+  [
+    `*Showing top ${winnerList.length} viewer${
+      winnerList.length === 1 ? '' : 's'
+    }.*`,
+    '',
+    '| Viewer | Wins |',
+    '| ------ | ---- |',
+    ...winnerList.map(
+      (winner: WinnerTotalType) => `| ${winner.name} | ${winner.total} |`
+    ),
+    ''
+  ].join('\n');
 
 export default leaderboardFormatter;
